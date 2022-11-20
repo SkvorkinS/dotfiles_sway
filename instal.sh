f7ad6179bfdcabde57551004201e6e9f16217935
skvorkins <skvorkins@gmail.com>
2022-11-20 20:56:29 +0600

    added Telegram in script

--- instal.sh
+++ instal.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pacman --noconfirm -S wayland xorg-xwayland foot waybar swaybg rofi ttf-iosevka-nerd pcmanfm pavucontrol firefox bitwarden leafpad gvfs libsixel grim slurp wl-clipboard swappy jq fish galculator neovim polkit lxsession lxappearance pulseaudio
+pacman --noconfirm -S wayland xorg-xwayland foot waybar swaybg rofi ttf-iosevka-nerd pcmanfm pavucontrol firefox bitwarden leafpad gvfs libsixel grim slurp wl-clipboard swappy jq fish galculator neovim polkit lxsession lxappearance pulseaudio telegram-desktop
 
 pacman --noconfirm -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 
